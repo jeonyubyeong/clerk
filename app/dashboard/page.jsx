a3ccc0cc7c3aa19823578125213d10c2b9ed743b
username <yubyeong01@naver.com>
2023-09-26 16:03:52 +0900

    웹프로그래밍실습

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const DashboardPage = () => {
   return (
     <div>
-      <h1 className='text-2xl font-bold mb-5'>Dashboard
-        (대시보드)</h1>
-        <p className='mb-5'>
-          Welcome to Dashboard. 로그인된 사용자의 정보를 보여줍니다.
-        </p>
+      <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+      <p className="mb-5">Welcome to Dashboard.</p>
     </div>
-  )
-}
-
-export default DashboardPage
+  );
+};
+export default DashboardPage;
